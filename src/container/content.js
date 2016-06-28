a8ce2f5d69cb06179d6808619bcf0d26f4c7ec8e
Lalit Chaturvedi <lalit.rhombus@gmail.com>
2016-06-28 14:18:50 +0530

    adding filters and content display initials

--- src/container/content.js
+++ src/container/content.js
@@ -7,6 +7,36 @@ import { Link } from 'react-router';
 class Content extends React.Component {
   constructor(props) {
     super(props);
+    this.updateView();
+  }
+
+  updateView = () => {
+      let average,
+          centuries,
+          matches,
+          runs,
+          data = this.props.dataState.data;
+
+      // performance anlysis
+      // let t0 = performance.now();
+
+      for (var key in data) {
+
+          let tempYears = moment(data[key].date, "DD MMM YYYY").get('year');
+          if (years.indexOf(tempYears) == -1) {
+              years.push(tempYears);
+          }
+
+          if (grounds.indexOf(data[key].ground) == -1) {
+              grounds.push(data[key].ground);
+          }
+
+          let tempTeam = data[key].opposition.substr(2);
+          if (teams.indexOf(tempTeam) == -1) {
+              teams.push(tempTeam);
+          }
+
+      }
   }
 
   render() {
@@ -21,19 +51,19 @@ class Content extends React.Component {
           </div>
           <div className="tab">
             <p className="text">
-              Matches
+              Runs
               <span className="count">200</span>
             </p>
           </div>
           <div className="tab selected">
             <p className="text">
-              Matches
+              Average
               <span className="count">200</span>
             </p>
           </div>
           <div className="tab">
             <p className="text">
-              Matches
+              Centuries
               <span className="count">200</span>
             </p>
           </div>
@@ -47,8 +77,8 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({featchData}, dispatch);
 }
 
-function mapStateToProps({data}){
-  return {data};
+function mapStateToProps({dataState}){
+  return {dataState};
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Content);
--- src/container/filters.js
+++ src/container/filters.js
@@ -38,20 +38,36 @@ class Filter extends React.Component {
     }
     else{
         listContent = list.map(list=>{
-        return  <p key={list} className="selected">{list}<svg><use xlinkHref={`public/iconSprite.svg#icon-user`}></use></svg></p>;
+        return  <p key={list} className="selected">{list}<svg onClick={this.onRemoveSelection.bind(this,list,sideOptionName)}><use xlinkHref={`public/iconSprite.svg#icon-user`}></use></svg></p>;
       });      
     }
     return <div className="selections"> {listContent}</div>;
   }
 
-  onSideOptionClick=(val,selectedOption)=>{
-    if(this.state[selectedOption].indexOf(val)==-1){
-      this.setState({selectedOption:this.state[selectedOption].push(val)});
+  showEmptyOption = (sideOptionName) =>{
+    if(this.state[sideOptionName].length>0){
+      return <p key="htf" onClick={this.onEmptySelection.bind(this,sideOptionName)} className="clearAll"><svg><use xlinkHref={`public/iconSprite.svg#icon-user`}></use></svg></p>;
+    }else{
+      return <span className="C-Hide"></span>;
     }
   }
 
+  onSideOptionClick=(val,selectedOption)=>{
+    if(this.state[selectedOption].indexOf(val)==-1){
+      this.setState({selectedOption:this.state[selectedOption].push(val)});
+    }else{
+      this.setState({selectedOption:this.state[selectedOption].splice(this.state[selectedOption].indexOf(val),1)});
+    }
+  }
   
-  
+  onRemoveSelection=(val,selectedOption)=>{
+    this.setState({selectedOption:this.state[selectedOption].splice(this.state[selectedOption].indexOf(val),1)});
+  }
+
+  onEmptySelection=(selectedOption)=>{
+    this.setState({selectedOption:this.state[selectedOption].splice(0,this.state[selectedOption].length)});
+  }
+
   render() {
     if(Object.keys(this.props.dataState).length<=0){
       return <div className="loading">Please Wait, Getharing Data....</div>;
@@ -70,34 +86,45 @@ class Filter extends React.Component {
                   <p className="text">Year</p>
                   {this.showSideOptions(this.props.dataState.years,"selectedYears")}
                   {this.showSelection(this.state.selectedYears,"selectedYears")}
+                  {this.showEmptyOption("selectedYears")}
                 </div>
                 <div className="filter">
                   <p className="text">Team</p>
                   {this.showSideOptions(this.props.dataState.teams,"selectedTeams")}
                   {this.showSelection(this.state.selectedTeams,"selectedTeams")}
+                  {this.showEmptyOption("selectedTeams")}
                 </div>
                 <div className="filter">
                   <p className="text">Grounds</p>
                   {this.showSideOptions(this.props.dataState.grounds,"selectedGrounds")}
                   {this.showSelection(this.state.selectedGrounds,"selectedGrounds")}
+                  {this.showEmptyOption("selectedGrounds")}
                 </div>
                 <div className="filter">
                   <p className="text">Result</p>
                   {this.showSideOptions(["won","lost","tied"],"selectedResults")}
                   {this.showSelection(this.state.selectedResults,"selectedResults")}
+                  {this.showEmptyOption("selectedResults")}
                 </div>
                 <div className="filter">
                   <p className="text">Innings</p>
                   {this.showSideOptions(["1st","2nd"],"selectedInnings")}
                   {this.showSelection(this.state.selectedInnings,"selectedInnings")}
+                  {this.showEmptyOption("selectedInnings")}
                 </div>
                 <div className="filter">
                   <p className="text">Toss</p>
                   {this.showSideOptions(["won","lost"],"selectedToss")}
                   {this.showSelection(this.state.selectedToss,"selectedToss")}
+                  {this.showEmptyOption("selectedToss")}
                 </div>
               </div>
-              <Content />
+              <Content selectedYears={this.state.selectedYears}
+                       selectedTeams={this.state.selectedTeams}
+                       selectedGrounds={this.state.selectedGrounds}
+                       selectedResults={this.state.selectedResults}
+                       selectedInnings={this.state.selectedInnings}
+                       selectedToss={this.state.selectedToss} />
             </div>
           </div>
       </div>
@@ -111,7 +138,6 @@ function mapDispatchToProps(dispatch) {
 
 function mapStateToProps({dataState}){
   // console.log(sizeof.sizeof(dataState,true));
-  console.log(dataState);
   return {dataState};
 }
 
